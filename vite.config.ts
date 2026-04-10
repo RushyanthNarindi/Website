a6375da47ac52ff788dbddebd1da8ab84b87541b
Rushyanth Narindi <rushyanthnarindi@gmail.com>
2026-04-10 00:11:03 -0500

    rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  base: '/website/',  // Match your repo name
   plugins: [react(), tsconfigPaths()],
 })
