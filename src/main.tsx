f4d4858a01cb85c9ed81e88320939c2b3d011f93
Rushyanth Narindi <rushyanthnarindi@gmail.com>
2026-04-10 00:57:57 -0500

    Add basename to BrowserRouter for GitHub Pages subpath

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import ThemeProvider from './ThemeProvider'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Website/">
       <ThemeProvider>
         <App />
       </ThemeProvider>
