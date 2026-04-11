0c836b48302c494808fd970296c4ac6a02ff1b1b
Rushyanth Narindi <rushyanthnarindi@gmail.com>
2026-04-11 01:32:53 -0500

    resolve vite errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
-import svgr from 'vite-plugin-svgr'
-
+export default defineConfig({
   base: '/Website/',  // Match your repo name (case-sensitive)
   plugins: [react(), tsconfigPaths(), svgr()],
-})
+});
