5320b59cc7fd2884ba670f211f569f8e7aac28c8
Rushyanth Narindi <rushyanthnarindi@gmail.com>
2026-04-11 01:23:33 -0500

    Logo Color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useTheme } from '../ThemeProvider'
-import LogoSvg from '../assets/images/WtxtBbg.svg'
+import { ReactComponent as LogoSvg } from '../assets/images/WtxtBbg.svg'
 
 export default function Header(){
   const { theme, toggleTheme } = useTheme()
@@ -10,7 +10,7 @@ export default function Header(){
     <header className="site-header">
       <div className="container header-row">
         <Link className="logo" to="/">
-          <img src={LogoSvg} alt="Logo" style={{ height: 32, verticalAlign: 'middle' }} />
+          <LogoSvg style={{ height: 32 }} />
         </Link>
 
         <nav className="nav" id="site-nav">
