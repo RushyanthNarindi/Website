9b720f70417bc4eaab734a2aecf1f5a8c168e22b
Rushyanth Narindi <rushyanthnarindi@gmail.com>
2026-04-11 00:21:53 -0500

    rebuild

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './styles.css'
-import ThemeProvider from './ThemeProvider'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles.css';
+import ThemeProvider from './ThemeProvider';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Website/">
+    <BrowserRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
