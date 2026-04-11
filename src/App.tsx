cebab7cef00d5634e0bfd90467c5eb1ae056550d
Rushyanth Narindi <rushyanthnarindi@gmail.com>
2026-04-11 12:04:23 -0500

    update file remane

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
 import About from './pages/About'
-import Resume from './pages/Resume'
+import Project from './pages/Project'
 import Contact from './pages/Contact'
 import Stats from './pages/Stats'
-import Article from './pages/Article'
 
 export default function App(){
   return (
@@ -16,10 +15,9 @@ export default function App(){
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
+          <Route path="/project" element={<Project />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/stats" element={<Stats />} />
-          <Route path="/writings/article-1" element={<Article />} />
         </Routes>
       </main>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,7 +32,7 @@ export default function Header(){
                 </Link>
               </li>
               <li>
-                <Link to="/resume" onClick={closeNav} className="nav-pill">
+                <Link to="/project" onClick={closeNav} className="nav-pill">
                   <svg viewBox="0 0 24 24" aria-hidden><path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8l-6-6zm-1 1.5L18.5 9H13V3.5zM6 20V4h5v7h7v9H6z"/></svg>
                   Project
                 </Link>
